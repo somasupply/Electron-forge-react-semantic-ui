7ee1e99c8acc2675cd3f80ee2b5052459bfc0942
Matthew <mr.matthewmorales@gmail.com>
2018-12-16 13:14:06 -0500

    Electron-Forge-React-Semantic-UI-Boilerplate
    Title says it all. Boilerplate for electron projects using electron forge's react template and semantic UI

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default class App extends React.Component {
+  render() {
+    return (<div>
+      <h2>Welcome to React!</h2>
+      <Button>Test button </Button>
+    </div>);
+  }
+}
